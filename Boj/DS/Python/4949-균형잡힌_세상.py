7184301141e180e372804fd0f7ce904283b5dbd1
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-06-13 18:25:24 +0900

    Pass: Boj 4949 균형잡힌 세상

---
+++ Boj/DS/Python/4949-균형잡힌_세상.py
@@ -0,0 +1,25 @@
+from sys import stdin
+from collections import deque
+
+
+while (True):
+    data = stdin.readline().rstrip()
+    stack = deque()
+    if data == ".":
+        break
+    for c in data:
+        if c == '(' or c == '[':
+            stack.append(c)
+        elif len(stack) and stack[-1] == '(' and c == ')':
+            stack.pop()
+        elif len(stack) and stack[-1] == '[' and c == ']':
+            stack.pop()
+        elif c ==']' or c == ')':
+            stack.append(c)
+        elif c == '.':
+            break
+    
+    if len(stack) == 0:
+        print("yes")
+    else:
+        print("no")
