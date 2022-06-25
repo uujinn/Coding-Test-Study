909c623424a0ee24264e291f92fa850f30db44dc
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-06-26 01:08:34 +0900

    Pass: Programmers 콜라츠 추측

--- Programmers/Level1/Python/콜라츠_추측.py
+++ Programmers/Level1/Python/콜라츠_추측.py
@@ -1,19 +1,15 @@
-def solution(n):
+def solution(num):
     answer = 0
 
-    if n == 1:
-        return 0
-        
-    while (n!=1):
-        if answer == 500:
-            print(-1)
-
-        if n % 2 == 0:
-            n /= 2
-
-        else:
-            n /= 3
-            n += 1
+    while answer < 500:
         answer += 1
+        if num % 2 == 0:
+            num = num / 2
+        else:
+            num = (num * 3) + 1
+        if num == 1:
+            break
+    if answer == 500:
+        answer = -1
 
     return answer
