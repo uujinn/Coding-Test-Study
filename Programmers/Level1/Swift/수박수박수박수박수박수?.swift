dc7097ee971e495359b04d4a2067c41a942ce903
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-08-23 16:22:19 +0900

    Revert "Non-Pass: Programmers 수박수박수박수박수박수? Swift"
    This reverts commit 8e3a3d908bfdc777b763bf9b62f054ae40fe5cdf.

--- Programmers/Level1/Swift/수박수박수박수박수박수?.swift
+++
@@ -1,7 +0,0 @@
-func solution(_ n:Int) -> String {
-    if n % 2 == 0{
-        return String(repeating: "수박", count: n / 2)
-    }else{
-        return String(repeating: "수박", count: n / 2) + "수"
-    }
-}
