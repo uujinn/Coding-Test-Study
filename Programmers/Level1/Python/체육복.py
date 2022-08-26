abc286ebd825f72db6579b283385ec9757e4aff4
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-08-26 15:52:27 +0900

    Pass: Programmers 체육복 Python

--- Programmers/Level1/Python/체육복.py
+++ Programmers/Level1/Python/체육복.py
@@ -1,25 +1,12 @@
 def solution(n, lost, reserve):
-    answer = 0
-    students = [1] * n 
+    # 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 
+    reserve_student = set(reserve) - set(lost) 
+    lost_student = set(lost) - set(reserve)
 
-    for i in range(n):
-        if (i+1) in lost:
-            students[i] -= 1
-        if (i+1) in reserve:
-            students[i] += 1
-         
-    for i in range(n):
-        if i != len(students)-1:
-            if students[i] > 1 and students[i+1] == 0:
-                students[i] -= 1
-                students[i+1] += 1
-            if i != 0:
-                if students[i] > 1 and students[i-1] == 0:
-                    students[i] -= 1
-                    students[i-1] += 1
-                
-    
-    for i in students:
-        if i > 0:
-            answer += 1
-    return answer
+    for i in reserve_student:
+        if i-1 in lost_student: # 왼쪽에 있는 학생이 체육복이 없다면 빌려줌
+            lost_student.remove(i-1)
+        elif i + 1 in lost_student: # 오른쪽 학생
+            lost_student.remove(i+1)
+
+    return n - len(lost_student)
