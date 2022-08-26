a7530c38a18ddc4cdb04f5103ec99a46f577ac6f
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-08-26 14:31:20 +0900

    Pass: 문자열 다루기 기본 Swift

--- Programmers/Level1/Swift/문자열_다루기_기본.swift
+++ Programmers/Level1/Swift/문자열_다루기_기본.swift
@@ -1,6 +1,6 @@
 func solution(_ s:String) -> Bool {
     
-    if (s.count == 4 || s.count == 6) && (Double(s) != nil){
+    if (s.count == 4 || s.count == 6) && (Int(s) != nil){
         return true
     }
     
