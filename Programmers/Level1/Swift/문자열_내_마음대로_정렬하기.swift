52071dd50da48427c1a02fae6788b4d18957d318
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-09-07 21:26:17 +0900

    Pass: Programmers 문자열 내 마음대로 정렬하기 Swift

---
+++ Programmers/Level1/Swift/문자열_내_마음대로_정렬하기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ strings:[String], _ n:Int) -> [String] {
+    let index = strings[0].index(strings[0].startIndex, offsetBy: n)
+    let result = strings.sorted(by: {(s1: String, s2: String) -> Bool in
+        if s1[index] == s2[index]{
+            return s1 < s2
+        }else{
+            return s1[index] < s2[index]
+        }               
+    })
+    return result
+}
