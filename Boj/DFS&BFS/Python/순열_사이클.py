6d1906aae52b42be8b0a7e6994605fa126906009
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-09-11 17:44:14 +0900

    Pass: Boj 순열 사이클 Python

--- Boj/DFS&BFS/Python/순열_사이클.py
+++ Boj/DFS&BFS/Python/순열_사이클.py
@@ -1,41 +1,32 @@
-from collections import deque
 import sys
-
+sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
 T = int(input())
+
+def dfs(v):
+  visited[v] = 1
+  n = vertex[v]
+
+  if visited[n] == 0:
+    dfs(n)
 
 for _ in range(T):
   N = int(input())
 
   arr = list(map(int, input().strip().split()))
-  vertex = [[0] * (N+1) for _ in range(N + 1)]
+  vertex = [0] * (N+1)
   visited = [0] * (N+1)
 
   answer = 0
 
   for idx, i in enumerate(arr):
-    vertex[idx+1][i] = 1
-
+    vertex[idx+1] = i
 
   for i in range(1, N+1):
-    # bfs
-    q = deque()
     if visited[i] == 0:
-      q.append(i)
-      while q:
-        v = q.popleft()
-        visited[v] = 1
-
-        for j in range(i+1, N+1):
-          if visited[j] == 0 and vertex[j][v] == 1:
-            visited[j] = 1
-            q.append(j)
+      dfs(i)
       answer += 1
-    else:
-      continue
-  
+
   print(answer)
 
-
-  
