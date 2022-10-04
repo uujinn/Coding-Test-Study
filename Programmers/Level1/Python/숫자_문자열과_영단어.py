e6e4c95a3a917c5ebefb2edc7c723a58e34368ad
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-10-05 01:22:44 +0900

    Pass: Programmers 숫자 문자열과 영단어 Python

---
+++ Programmers/Level1/Python/숫자_문자열과_영단어.py
@@ -0,0 +1,8 @@
+def solution(s):
+    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for i in range(len(words)):
+        s = s.replace(words[i], str(i))
+
+    return int(s)
+
