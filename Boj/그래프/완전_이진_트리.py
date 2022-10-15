2af304ec06d1d6650b34f6f5b2fec595f02988b9
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-10-16 01:22:27 +0900

    Pass: Boj 완전 이진 트리 Python

---
+++ Boj/그래프/완전_이진_트리.py
@@ -0,0 +1,22 @@
+K = int(input())
+arr = list(map(int, input().split()))
+level = [[] for _ in range(K)]
+
+def recur(tree, cnt):
+    root = tree[len(tree)//2]
+
+    level[cnt].append(root)
+
+    left = tree[:len(tree)//2]
+    right = tree[len(tree)//2+1:]
+
+    if len(tree) == 1:
+        return level
+    else:
+        recur(left, cnt + 1)
+        recur(right, cnt + 1)
+    return level
+
+
+for l in recur(arr, 0):
+    print(*l)
